419eaf4e66ababd83cb6d6a8cea082b82b690f7d
Pyatov Maxim <maxs.pyatov36@mail.ru>
2017-03-03 19:17:47 +0300

    Added ActionBarDrawerToggle.

--- app/src/main/java/com/test/maxp36/todo/MainActivity.java
+++ app/src/main/java/com/test/maxp36/todo/MainActivity.java
@@ -3,6 +3,7 @@ package com.test.maxp36.todo;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -38,5 +39,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_toggle_open, R.string.drawer_toggle_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        /*NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        })*/
     }
 }
